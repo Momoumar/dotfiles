4cda058944087960a8ef92c4fb54c7eeb93b5602
guttertec <guttertec@Metaversum.local>
2014-01-17 16:59:47 +0100

    Add a casks to brewfile and generated a try for requirements.

---
+++ brewfile-requirements.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# originally from Ryusuke Kimura - https://github.com/ryurock/brewfile/blob/
+
+set -e
+
+if [ ! `uname` = "Darwin" ]; then
+  echo "Sorry. falco is only available only OSX"
+  exit
+fi
+
+# check Xcode
+if ! which xcodebuild >/dev/null 2>&1; then
+  echo "[Error] Dependency Error. not installed XCode and XCode CommandLine Tools"
+  exit
+else
+    echo "[Install] Already exists dependency XCode and XCode CommandLine Tools"
+fi
+
+# Homebrew installation
+if ! which brew >/dev/null 2>&1; then
+  echo "[Install] Dependency Homebrew"
+  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+  command_ver=$(brew --version)
+  command=$(which brew)
+  echo "[Install] installed Homebrew. path : ${comand} version : ${command_ver}"
+else
+    echo "[Install] Already exists dependency Homebrew"
+fi
+
+# check Ruby is installed
+if ! which ruby >/dev/null 2>&1; then
+  echo "[Error] Dependency Error. not installed Ruby"
+  exit
+fi
+
+# check Rubygems is installed
+if ! which gem >/dev/null 2>&1; then
+  echo "[Error] Dependency Error. not installed Ruby Gem"
+  exit
+fi
+
+# Brewdler installation
+if ! which brewdle >/dev/null 2>&1; then
+  echo "[Install] Dependency Brewdle"
+  gem install brewdler --no-ri --no-rdoc
+  command_ver=$(brewdle --version)
+  command=$(which brewdle)
+  echo "[Install] installed Brewdle. path : ${comand} version : ${command_ver}"
+else
+  echo "[Install] Already exists dependency Brewdle"
+fi
+
+echo "Dependency Syntax OK"
